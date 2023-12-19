7b39c8e81c52fafc5ebab9e5cf6be235a00bcaf7
oldjang <oldjang@outlook.com>
2023-12-19 21:14:04 +0800

    test

---
+++ HighDSkipList.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Mengxu Jiang on 19/12/2023.
+//
+
+#ifndef SIGMOD16_HIGHDSKIPLIST_HPP
+#define SIGMOD16_HIGHDSKIPLIST_HPP
+
+
+class HighDSkipList {
+
+};
+
+
+#endif //SIGMOD16_HIGHDSKIPLIST_HPP
